5fb4bb30469c8247617d296834bb6ee3ba452995
Kangyun Wang <wky@171.215.109.145.hva.nl>
2018-05-18 13:18:01 +0200

    Add styled components

--- app/index.js
+++ app/index.js
@@ -7,10 +7,15 @@
 import React, { Component } from 'react';
 import {
   Platform,
-  StyleSheet,
   Text,
   View
 } from 'react-native';
+
+import styled from 'styled-components';
+
+const StyledText = styled.Text`
+	color: red;
+`;
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -24,36 +29,17 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
+      <View>
+        <StyledText>
           Welcome to React Native
-        </Text>
-        <Text style={styles.instructions}>
+        </StyledText>
+        <StyledText>
           To get started, edit App
-        </Text>
-        <Text style={styles.instructions}>
+        </StyledText>
+        <StyledText>
           {instructions}
-        </Text>
+        </StyledText>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
