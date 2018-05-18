7c1e2cd70772d9fa15495eb0616e7677ee0c36e9
Sam-Guliker <samguliker@hotmail.com>
2018-05-18 13:19:02 +0200

    Whats up

---
+++ app/reminder.js
@@ -0,0 +1,63 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Button
+} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={}>
+        <Text style={>
+        Make sure your device is plugged in to your charger.
+        Do not close the app.
+        </Text>
+        <Text style={}>
+          make sure you are lying down or reclined in a chair
+        </Text>
+        <Button style={}>
+        next
+        </Button>
+      </View>
+    );
+  }
+}
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+//   button: {
+//     font-size: 40
+//   }
+// });
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { AppRegistry } from 'react-native';
-import App from './app';
+import App from './app/reminder';
+
 
 AppRegistry.registerComponent('futureOfSleep', () => App);
